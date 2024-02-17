3483707e06945bd33b80157b1a34626166368192
Muhammad Sinar Agusta <147012035+Yyaksha@users.noreply.github.com>
2024-02-17 10:51:27 +0700

    Update konversi-suhu.py

--- Muhammad Sinar Agusta - Tugas Algoritma & Struktur Data/Pemrograman/Tugas 1/konversi-suhu.py
+++ Muhammad Sinar Agusta - Tugas Algoritma & Struktur Data/Pemrograman/Tugas 1/konversi-suhu.py
@@ -7,6 +7,6 @@ Ce = 5/4 * Reamur
 Fa = 9/4 * Reamur + 32
 Kel = Reamur * 5/4 + 273.15
 
-print("Suhu dari Reamur %2.f --> Celcius : %.2f"% (Ce))
-print("Suhu dari Reamur %2.f --> Fahrenheit : %.2f"% (Fa))
-print("Suhu dari Reamur %2.f --> Kelvin : %.2f"% (Kel))
+print("Suhu dari Reamur --> Celcius : %.2f"% Ce)
+print("Suhu dari Reamur --> Fahrenheit : %.2f"% Fa)
+print("Suhu dari Reamur --> Kelvin : %.2f"% Kel)
